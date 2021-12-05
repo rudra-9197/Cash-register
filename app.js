bfd5eea4101cffa1b8fe3e5903ade3c0674e72cd
rudra-9197 <91685979+rudra-9197@users.noreply.github.com>
2021-12-05 23:04:14 +0530

    logic

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-const billAmountValue =document.querySelector(#billAmount);
-console.log(billAmountValue);
+var billAmount = document.querySelector("#bill-Amount");
+var checkButton = document.querySelector("#check-Button");
+var cashGiven = document.querySelector("#cash-Given");
+var errorMessage = document.querySelector("#alert-Message");
+var givenNote = document.querySelectorAll(".no-Notes");
+
+var valueOfNotes = [2000, 500, 200, 100, 50, 20, 10, 1];
+
+function clickerevent() {
+  if (billAmount.value > 0) {
+    if (cashGiven.value >= billAmount.value) {
+      changeGiven = cashGiven.value - billAmount.value;
+      for (i = 0; i < valueOfNotes.length; i++) {
+        noofNotes = Math.trunc(changeGiven / valueOfNotes[i]);
+        changeGiven = changeGiven % valueOfNotes[i];
+        console.log(valueOfNotes[i], noofNotes);
+        givenNote[i].innerText = noofNotes;
+      }
+    } else {
+      errorMessage.innerText = " Cash given should be greater than Bill Amount";
+    }
+  } else {
+    errorMessage.innerText = "Bill Amount should be greater than 0 ";
+  }
+}
+
+checkButton.addEventListener("click", clickerevent);
